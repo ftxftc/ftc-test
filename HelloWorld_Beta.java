1234733de551c13edf4dc8e983f6ba7c995ec9a2
Alpha <brightscholars@gmail.com>
2025-09-05 16:12:15 -0500

    Commit from testbr2

--- HelloWorld_Beta.java
+++ HelloWorld_Beta.java
@@ -1,7 +1,7 @@
-public class HelloWorld_Alpha {
+public class HelloWorld_Beta {
     public static void main(String[] args) {
         System.out.println("Hello, World from Alpha!");
 	System.out.println("Hello, World from Puneet from System1!");
-	System.out.println("Hello, World from Alpha from Second system!");
+	System.out.println("Hello, World from Beta system! added by Pari/testbr1");
     }
 }
